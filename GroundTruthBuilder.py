af429ee1248b5ddb74162432bc10da77a61720dc
Emilio Lopez <emilio.lova@gmail.com>
2020-02-06 03:15:57 +0100

    Added zoom and controls move image

--- GroundTruthBuilder.py
+++ GroundTruthBuilder.py
@@ -1,10 +1,9 @@
 import glob, os
-from functools import partial
 from PIL import Image, ImageDraw
 from kivy.app import App
 from kivy.properties import NumericProperty, ListProperty, \
-        BooleanProperty, StringProperty, ObjectProperty
-from kivy.uix.boxlayout import BoxLayout
+		BooleanProperty, StringProperty, ObjectProperty
+from kivy.uix.stacklayout import StackLayout
 from kivy.lang import Builder
 from kivy.graphics import Line
 from kivy.config import Config
@@ -15,8 +14,9 @@ from kivy.event import EventDispatcher
 from kivy.clock import Clock
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+# Config.set('graphics', 'fullscreen', True)
 
-class LinePlay(BoxLayout):
+class LinePlay(StackLayout):
 
 	path = ''
 	# General Properties
@@ -39,10 +39,22 @@ class LinePlay(BoxLayout):
 
 	# Text vanish
 	tex_control = StringProperty("")
+
+	# Move corretion factor
+	x_factor = 0
+	y_factor = 0
+
+	# Variables to save
+	ori_size = ListProperty([])
+	zoom_val = 0
 	
 	# Functions
-	def __init__(self):
-		super(LinePlay, self).__init__()
+	def __init__(self, **kwargs):
+		super(LinePlay, self).__init__(**kwargs)
+
+		# Keyboard isten
+		self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+		self._keyboard.bind(on_key_down=self._on_keyboard_down)
 
 		# Iniciate Clock function
 		Clock.schedule_interval(self.update_path,.1)
@@ -62,6 +74,12 @@ class LinePlay(BoxLayout):
 				self.image_car.size = im_size.size
 				self.disa = False
 
+				# Reiniciate factors
+				self.x_factor = 0
+				self.y_factor = 0
+				self.ori_size = self.image_car.size
+				self.zoom_val = 0
+
 			self.lis.pop(0)
 
 	def changeimage(self, value):
@@ -70,32 +88,33 @@ class LinePlay(BoxLayout):
 		im_size = Image.open(self.img)
 		self.image_car.size = im_size.size
 
+		# Reiniciate factors
+		self.x_factor = 0
+		self.y_factor = 0
+		self.zoom_val = 0
+
 	def on_touch_down(self, touch):
 		if super(LinePlay, self).on_touch_down(touch):
 			return True
 
-		# Scrolling detection, changing image	
+		# Scrolling detection for zoom in and zoom out	
 		if touch.is_mouse_scrolling:
 			if touch.button == 'scrolldown':
-				if self.slider_max.max > self.slider_max.value:
-					self.slider_max.value = self.slider_max.value + 1
+				self.zoom_in()
 				return True
-			
+
 			elif touch.button == 'scrollup':
-				if self.slider_max.min < self.slider_max.value:
-					self.slider_max.value = self.slider_max.value - 1
+				self.zoom_out()
 				return True
 
 		# Grab Clicks
 		touch.grab(self)
 		self.points.append(touch.pos)
-		self.lpoints.append((touch.x - self.image_car.pos[0], self.image_car.size[1] - (touch.y - self.image_car.pos[1])))
 		return True
 
 	def on_touch_move(self, touch):
 		if touch.grab_current is self:
 			self.points[-1] = touch.pos
-			self.lpoints[-1] = (touch.x - self.image_car.pos[0], self.image_car.size[1] - (touch.y - self.image_car.pos[1]))
 			return True
 		return super(LinePlay, self).on_touch_move(touch)
 
@@ -103,13 +122,179 @@ class LinePlay(BoxLayout):
 		if touch.grab_current is self:
 			touch.ungrab(self)
 			return True
-		return super(LinePlay, self).on_touch_up(touch)   
+		return super(LinePlay, self).on_touch_up(touch)
 
-	def save_image(self, size, pos):
+	# keyboard controler
+	def _keyboard_closed(self):
+		self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+		self._keyboard = None
+
+	def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+		move_value = 40
+
+		if keycode[1] == 'up':
+			self.zoom_in()
+		elif keycode[1] == 'down':
+			self.zoom_out()
+		elif keycode[1] == 'right':
+			# Change to next image
+			if self.slider_max.value < self.slider_max.max:
+				self.slider_max.value += 1 
+		elif keycode[1] == 'left':
+			# Change to before image
+			if self.slider_max.value > self.slider_max.min:
+				self.slider_max.value -= 1
+		elif keycode[1] == 'w' or 's' or 'a' or 'd':
+			self.move_in(keycode[1])
+
+	# Zoom in and out functions
+	def zoom_in(self):
+
+		# Update list points zoom in
+		self.points = [(x[0] - (self.image_car.center_x - x[0]) + self.x_factor, x[1] - (self.image_car.center_y - x[1]) + self.y_factor) for x in self.points]
+		
+		# Update final point lists
+		self.final_points = [[(x[0] - (self.image_car.center_x - x[0]) + self.x_factor, x[1] - (self.image_car.center_y - x[1]) + self.y_factor) for x in y] for y in self.final_points]
+		
+		# Removing painted lines
+		self.canvas.remove_group('Lines')
+
+		# Adding new Lines
+		for i in range(0,len(self.final_points)):
+			with self.canvas:
+				Line(points=self.final_points[i], group='Lines')
+
+		# Update image size and pos (Handling updated error)
+		self.image_car.pos[0] = self.image_car.pos[0] - self.image_car.size[0] / 2
+		self.image_car.pos[1] = self.image_car.pos[1] - self.image_car.size[1] / 2
+
+		self.image_car.size[0] = self.image_car.size[0] * 2
+		self.image_car.size[1] = self.image_car.size[1] * 2
+
+		self.anla_ya.do_layout()
+
+		# Reiniciate factors
+		self.x_factor = 0
+		self.y_factor = 0
+		self.zoom_val += 1
+
+	def zoom_out(self):
+
+		# Update list points zoom out
+		self.points = [(((self.image_car.center_x + x[0])/2) + self.x_factor, ((self.image_car.center_y + x[1])/2) + self.y_factor) for x in self.points]
+		
+		# Update final point lists
+		self.final_points = [[(((self.image_car.center_x + x[0])/2) + self.x_factor, ((self.image_car.center_y + x[1])/2) + self.y_factor) for x in y] for y in self.final_points]
+		
+		# Removing painted lines
+		self.canvas.remove_group('Lines')
+
+		# Adding new Lines
+		for i in range(0,len(self.final_points)):
+			with self.canvas:
+				Line(points=self.final_points[i], group='Lines')
+
+		# Update image and pos size (Because updated error)
+		self.image_car.size[0] = self.image_car.size[0] / 2
+		self.image_car.size[1] = self.image_car.size[1] / 2
+
+		self.image_car.pos[0] = self.image_car.size[0] / 2 + self.image_car.pos[0]
+		self.image_car.pos[1] = self.image_car.size[1] / 2 + self.image_car.pos[1]
+
+		self.anla_ya.do_layout()
+		
+		# Reiniciate factors
+		self.x_factor = 0
+		self.y_factor = 0
+		self.zoom_val -= 1
+
+	# Move image to x positions updating the list points
+	def move_in(self, param):
+		if param == 'w':
+			self.image_car.pos[1] = self.image_car.pos[1] + 40
+
+			# Update list points move
+			self.points = [(x[0], x[1] + 40) for x in self.points]
+			# Update final list points
+			self.final_points = [[(x[0], x[1] + 40) for x in y] for y in self.final_points]
+			# Update y factor
+			self.y_factor -= 40
+
+		elif param == 's':
+			self.image_car.pos[1] = self.image_car.pos[1] - 40
+
+			# Update list points move
+			self.points = [(x[0], x[1] - 40) for x in self.points]
+			# Update final list points
+			self.final_points = [[(x[0], x[1] - 40) for x in y] for y in self.final_points]
+			# Update y factor
+			self.y_factor += 40
+
+		elif param == 'd':
+			self.image_car.pos[0] = self.image_car.pos[0] + 40
+
+			# Update list points move
+			self.points = [(x[0] + 40, x[1]) for x in self.points]
+			# Update final list points
+			self.final_points = [[(x[0] + 40, x[1]) for x in y] for y in self.final_points]
+			# Update x factor
+			self.x_factor -= 40
+
+		elif param == 'a':
+			self.image_car.pos[0] = self.image_car.pos[0] - 40
+
+			# Update list points move
+			self.points = [(x[0] - 40, x[1]) for x in self.points]
+			# Update final list points
+			self.final_points = [[(x[0] - 40, x[1]) for x in y] for y in self.final_points]
+			# Update x factor
+			self.x_factor += 40
+
+		# Removing painted lines
+		self.canvas.remove_group('Lines')
+
+		# Adding new Lines
+		for i in range(0,len(self.final_points)):
+			with self.canvas:
+				Line(points=self.final_points[i], group='Lines')
+
+	def new_line(self):
+		# Handling empty list error
+		if not self.points:
+			self.close = False # Reiniciate close line
+			return
+
+		# Final point list
+		if self.close == True:
+			self.points.append(self.points[0])
+		
+		self.final_points.append(self.points)
+		self.points = []
+		self.close = False # Reiniciate close line
+
+		for i in range(0,len(self.final_points)):
+			with self.canvas:
+				Line(points=self.final_points[i], group='Lines')
+
+	def save_image(self):
 
 		# Generated mask
-		final_image = Image.new("RGB", [int(round(size[0])),int(round(size[1]))])
+		final_image = Image.new("RGB", [int(round(self.ori_size[0])),int(round(self.ori_size[1]))])
 		draw = ImageDraw.Draw(final_image)
+
+		# Updating image to default position
+		copy = self.zoom_val
+		if copy > 0:
+			for val in range(0, copy):
+				print(self.x_factor, self.y_factor)
+				self.zoom_out()
+		elif copy < 0:
+			for val in range(0, abs(copy)):
+				self.zoom_in()
+
+		# Updating last points in list
+		self.lpoints = [(x[0] - self.image_car.pos[0], self.image_car.size[1] - (x[1] - self.image_car.pos[1])) for x in self.points]
+		self.final_lpoints = [[(x[0] - self.image_car.pos[0], self.image_car.size[1] - (x[1] - self.image_car.pos[1])) for x in y] for y in self.final_points]
 
 		# Drawing lines
 		if not self.lpoints or len(self.lpoints) == 1:
@@ -135,28 +320,9 @@ class LinePlay(BoxLayout):
 		filename = filename + self.img[self.img.find("\\"): ]
 		filename = filename.replace(".jpg", "_mask.jpg").replace(".BMP", "_mask.BMP").replace(".png", "_mask.png").replace(".tiff", "_mask.tiff")
 		final_image.save(filename)
-		# print("size: ", size, " pos: ", pos, self.points, self.lpoints)
 
-	def new_line(self):
-		# Handling empty list error
-		if not self.points:
-			self.close = False # Reiniciate close line
-			return
-
-		# Final point list
-		if self.close == True:
-			self.points.append(self.points[0])
-			self.lpoints.append(self.lpoints[0])
-		
-		self.final_points.append(self.points)
-		self.final_lpoints.append(self.lpoints)
-		self.points = []
-		self.lpoints = []
-		self.close = False # Reiniciate close line
-
-		for i in range(0,len(self.final_points)):
-			with self.canvas:
-				Line(points=self.final_points[i], group='Lines')
+		# Reiniciate zoom_val
+		self.zoom_val = 0
 
 
 class GroundTruthBuilder(App):
@@ -165,5 +331,5 @@ class GroundTruthBuilder(App):
 
 
 if __name__ == '__main__':
-	Window.maximize() # Just work in windows?
+	# Window.maximize() # Just work in windows?
 	GroundTruthBuilder().run()
